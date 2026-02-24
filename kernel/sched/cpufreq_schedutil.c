ff9e6cf69fd5e9c6fa41395729984190ce6954a6
Gaurav Koli <79272711+koligaurav462@users.noreply.github.com>
2026-02-24 11:12:43 +0000

    Revert "cpufreq_schedutil: simple flat +25% dvfs headroom boosting"
    This reverts commit 7b63ff81c88848be8f643cf33e27bb836998515c.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -230,14 +230,31 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 
 static inline unsigned long apply_dvfs_headroom(unsigned long util, int cpu)
 {
-    unsigned long cap = capacity_orig_of(cpu);
+	unsigned long capacity = capacity_orig_of(cpu);
+	unsigned long headroom;
 
-	if (util < (cap >> 4)) // suppress noise < ~6.25%
-        return util;
+	/*
+	 * Skip boosting for very low utilization (< 6.25%)
+	 */
+	if (likely(util < (capacity >> 4)))
+		return util;
 
-    util += util >> 2; // flat 25% headroom
+	/*
+	 * Perform 12.5% boost in < 50% load and 25% boost in >= 50% load
+	 */
+	if (util < (capacity >> 1))
+		headroom = util >> 3;
+	else
+		headroom = util >> 2;
 
-    return min(util, cap);
+	/*
+	 * Ensure the total boosted utilization does not exceed the CPU's
+	 * maximum capacity
+	 */
+	if (util + headroom > capacity)
+		return capacity;
+
+	return util + headroom;
 }
 
 unsigned long sugov_effective_cpu_perf(int cpu, unsigned long actual,
