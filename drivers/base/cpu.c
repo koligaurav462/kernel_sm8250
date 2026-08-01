02a697413cfb345cf80ba2d507a2ed9c013dbc20
kvsnr113 <kvsnrprojkt113@gmail.com>
2026-08-01 16:25:09 +0700

    drivers/base: cpu: don't allow write to cpu_subsys_offline
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -69,7 +69,7 @@ static int cpu_subsys_online(struct device *dev)
 
 static int cpu_subsys_offline(struct device *dev)
 {
-	return cpu_down(dev->id);
+	return -EPERM;
 }
 
 void unregister_cpu(struct cpu *cpu)
