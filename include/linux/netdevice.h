d96a926ea19ed7c3191e6e59b8b431817f5d73de
deu <fawwazzuladhim700@gmail.com>
2026-08-02 16:45:50 +0700

    include: net: add missing xdp_umem structure
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>
    
    [rewrite commit msg from pull request]
    Signed-off-by: kvsnr113 <kvsnrprojkt113@gmail.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -602,6 +602,10 @@ struct netdev_queue {
 	struct dql		dql;
 #endif
 
+#ifdef CONFIG_XDP_SOCKETS
+	struct xdp_umem		*umem;
+#endif
+
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
@@ -720,6 +724,10 @@ struct netdev_rx_queue {
 	struct kobject			kobj;
 	struct net_device		*dev;
 	struct xdp_rxq_info		xdp_rxq;
+
+#ifdef CONFIG_XDP_SOCKETS
+	struct xdp_umem			*umem;
+#endif
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
